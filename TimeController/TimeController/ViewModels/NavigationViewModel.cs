86df3b9edd435fbc7e773dce8515f7ddbaddcffb
张欣茹 <602536660@qq.com>
2025-04-17 23:25:38 +0800

    feat:搭建项目框架

---
+++ TimeController/TimeController/ViewModels/NavigationViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using TimeController.Models;
+using System.Windows.Controls;
+using TimeController.Views.CasualMode;
+using TimeController.Views.Review;
+using TimeController.Views.StrongGoalWeek;
+using TimeController.Views.StrongGoalMonth;
+
+namespace TimeController.ViewModels
+{
+    public class NavigationViewModel
+    {
+        private readonly Frame _navigationFrame;
+        public ICommand NavigateCommand { get; private set; }
+
+        public NavigationViewModel(Frame navigationFrame)
+        {
+            _navigationFrame = navigationFrame;
+            NavigateCommand = new RelayCommand<string>(NavigateTo);
+        }
+
+        private void NavigateTo(string tag)
+        {
+            Page pageToLoad = tag switch
+            {
+                "CasualMode" => new CasualModeView(),
+                "MonthView" => new MonthView(),
+                "WeekView" => new WeekView(),
+                "Review" => new ReviewView(),
+                //"settings" => 
+                //"about" => 
+                _ => new CasualModeView()
+            };
+
+            _navigationFrame.Navigate(pageToLoad);
+        }
+    }
+
+    public class NavigationItem
+    {
+        public string Title { get; set; }
+        public string Tag { get; set; }
+
+        public NavigationItem(string title, string tag)
+        {
+            Title = title;
+            Tag = tag;
+        }
+    }
+}
---
+++ TimeController/TimeController/Views/Navigation/ShellView.xaml.cs
@@ -0,0 +1,65 @@
+﻿using iNKORE.UI.WPF.Modern.Controls;
+using TimeController.Views.CasualMode;
+using TimeController.Views.StrongGoalWeek;
+using TimeController.Views.StrongGoalMonth;
+using TimeController.Views.Review;
+using System;
+using System.Windows;
+
+namespace TimeController.Views.Navigation
+{
+    public partial class ShellView : System.Windows.Controls.UserControl
+    {
+        // 预先创建页面实例
+        public CasualModeView Page_CasualMode = new CasualModeView();
+        public WeekView Page_WeekView = new WeekView();
+        public MonthView Page_MonthView = new MonthView();
+        public ReviewView Page_Review = new ReviewView();
+        //public SettingsView Page_Settings = new SettingsView();
+        //public AboutView Page_About = new AboutView();
+
+        public ShellView()
+        {
+            InitializeComponent();
+            // 设置默认选中项
+            NavigationView_Root.SelectedItem = NavigationViewItem_CasualMode;
+        }
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            var item = sender.SelectedItem as NavigationViewItem;
+            Page page = null;
+
+            if (item == NavigationViewItem_CasualMode)
+            {
+                page = Page_CasualMode;
+            }
+            else if (item == NavigationViewItem_WeekView)
+            {
+                page = Page_WeekView;
+            }
+            else if (item == NavigationViewItem_MonthView)
+            {
+                page = Page_MonthView;
+            }
+            else if (item == NavigationViewItem_Review)
+            {
+                page = Page_Review;
+            }
+            else if (item == NavigationViewItem_Settings)
+            {
+                //page = Page_Settings;
+            }
+            else if (item == NavigationViewItem_About)
+            {
+                //page = Page_About;
+            }
+
+            if (page != null)
+            {
+                NavigationView_Root.Header = page.Title;
+                ContentFrame.Navigate(page);
+            }
+        }
+    }
+}
